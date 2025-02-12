e5e6cbad2f4cd5adfb243030b26c9c2d31b1c63b
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-12 14:26:42 +0000

    update files

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,27 +2,28 @@
 class Notes {
 	public static void main(String[] args) {
 
-		//if statement
-
 		int num;
-		num = 5;
+		num = -1;
 
-		int num3 = 0;//setup a default value for the variable
-		
-		if (num < 10) {
-			System.out.println(num + " was printed becuase if was true.");
-			num3 = 5;
-			int num2 = 9; //a variable created in an if statement, only exists inside that if statement
-			//this is the case for any curly brackets
+		int num2 = 0; //default value given to num2
+
+		if (num < 10 && num > 0) {
+			//any code written in an if statement is skipped when that condition is false
+			System.out.println(num + " was less than 10");
+			num2 = 9; 
+			//a variable created in an if statement, can only be used inside that if statement
 		}
-		//code cannot be written here.  Your program will crash
-		else { //when the condition(s) above is false
-			//num >= 10
-			System.out.println(num + " was printed becuase if was false.");
+		// System.out.println();
+		//YOU CANNOT CODE BETWEEN AN IF AND AN ELSE
+		// num >= 10 || num <= 0
+		else { //gets skipped when the if statement was true
+			System.out.println("This runs when the condition is false");
 		}
 
-		// System.out.println(num2);
-		System.out.println(num3);
+		System.out.println(num2);
+		System.out.println("after the if");
+
+
 
 	}
 }
